9876c7a21a0697beed48b19d557494b5c5816d99
Mooobi <antod123@naver.com>
2023-07-02 14:32:10 +0900

    초기환경셋팅

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { Provider } from 'react-redux'
-import { QueryClient, QueryClientProvider } from 'react-query'
-import { BrowserRouter } from 'react-router-dom'
-import { store } from './common/store/RootStore.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { Provider } from 'react-redux';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { BrowserRouter } from 'react-router-dom';
+import { store } from './common/store/RootStore.tsx';
 
 const queryClient = new QueryClient();
 
@@ -19,4 +19,4 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
       </QueryClientProvider>
     </Provider>
   </React.StrictMode>,
-)
+);
