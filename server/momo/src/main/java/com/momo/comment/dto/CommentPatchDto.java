3f32d54a3338c5832632f7fc11437e4404436f4b
myoungin <myoungin729@gmail.com>
2023-07-04 16:13:11 +0900

    Test: CommentService 테스트코드 작성

--- server/momo/src/main/java/com/momo/comment/dto/CommentPatchDto.java
+++ server/momo/src/main/java/com/momo/comment/dto/CommentPatchDto.java
@@ -1,8 +1,10 @@
 package com.momo.comment.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class CommentPatchDto {
     private Long memberId;
     private String content;
--- server/momo/src/main/java/com/momo/comment/dto/CommentPostDto.java
+++ server/momo/src/main/java/com/momo/comment/dto/CommentPostDto.java
@@ -2,8 +2,10 @@ package com.momo.comment.dto;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class CommentPostDto {
     private Long memberId;
