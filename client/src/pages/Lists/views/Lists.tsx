5716805db9aef32df2d75ca1418e0ace87f025a4
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2023-07-04 09:49:55 +0900

    Chore: 컴포넌트파일생성과 아이콘 이미지 다운

---
+++ client/src/pages/Lists/views/Lists.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export default function Lists() {
+    return(
+        <Wrapper>
+
+        </Wrapper>
+    )
+}
+
+const Wrapper = styled.div`
+    display:flex;
+`
