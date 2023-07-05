f2212a946d12cf98c866dce2e92589c5b49ad3a0
rktdngud <dngud915@gmail.com>
2023-07-06 08:57:48 +0900

    Fix: 로그인 로직 변경

---
+++ server/momo/src/main/java/com/momo/security/config/SecurityConfig.java
@@ -0,0 +1,61 @@
+package com.momo.security.config;
+
+import com.momo.member.repository.MemberRepository;
+import com.momo.security.service.MemberDetailsService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final MemberRepository memberRepository;
+
+    public SecurityConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .headers().frameOptions().sameOrigin() // 설정 추가된 부분
+                .and()
+                .csrf().disable()
+                .formLogin()
+                .loginPage("/auths/login-form")
+                .loginProcessingUrl("/process_login")
+                .failureUrl("/auths/login-form?error")
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/")
+                .and()
+                .exceptionHandling().accessDeniedPage("/auths/access-denied")
+                .and()
+                .authorizeHttpRequests(authorize -> authorize
+                        .antMatchers("/members/register").permitAll()
+                        .antMatchers("/members/**").hasRole("ADMIN")
+                        .antMatchers("/**").permitAll()
+                );
+
+        return http.build();
+    }
+
+}
