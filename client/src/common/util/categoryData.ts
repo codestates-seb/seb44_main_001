ee3f2dc55560e9540daa28b23306cb529d86d127
Mooobi <antod123@naver.com>
2023-07-05 09:47:46 +0900

    Feat: 카테고리 드롭다운 추가

---
+++ client/src/common/util/categoryData.ts
@@ -0,0 +1,12 @@
+import { CategoryData } from './enums';
+
+export const categoryData: CategoryData[] = [
+  CategoryData.Pet,
+  CategoryData.Sports,
+  CategoryData.Study,
+  CategoryData.Music,
+  CategoryData.Game,
+  CategoryData.Food,
+  CategoryData.Culture,
+  CategoryData.Other,
+];
