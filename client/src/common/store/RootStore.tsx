1ef6f95562fd6fce7c406eb0ed6e02622151af65
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2023-07-06 13:24:56 +0900

    Chore: 충돌코드 삭제

--- client/src/common/store/RootStore.tsx
+++ client/src/common/store/RootStore.tsx
@@ -2,7 +2,6 @@ import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { locationSlice } from './LocationStore';
 import { createdPostSlice } from '../../pages/Write/store/CreatedPost';
 import { currentCategorySlice } from '../../pages/Lists/store/CurrentCategory';
-import { filteredListSlice } from '../../pages/Lists/store/FilteredList';
 import { setSignupSlice } from '../../pages/Signup/store/createSignupUser';
 import { categorySlice } from './CategoryStore';
 
@@ -10,10 +9,8 @@ import { categorySlice } from './CategoryStore';
 const rootReducer = combineReducers({
   location: locationSlice.reducer,
   createdPost: createdPostSlice.reducer,
-  filteredList : filteredListSlice.reducer,
   signup: setSignupSlice.reducer,
   currentCategory : currentCategorySlice.reducer,
-  filteredList: filteredListSlice.reducer,
   category: categorySlice.reducer,
 });
 
