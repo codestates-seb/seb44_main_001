153be1d8038b64a78d6c3044ce051011884cd3ef
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2023-07-07 12:29:31 +0900

    Feat: 404 page 완성

--- client/src/pages/NotFound/Views/NotFound.tsx
+++ client/src/pages/NotFound/Views/NotFound.tsx
@@ -1,3 +1,20 @@
+import { styled } from 'styled-components';
+import Logo from '../../../common/assets/logo/MOMO.png';
+import NotFound404 from '../../../common/assets/images/NotFound404.png';
 export default function NotFound() {
-  return <></>;
+  return (
+    <Wrapper>
+      <img src={Logo} alt="logo" />
+      <img src={NotFound404} alt="NotFound404" />
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+`
