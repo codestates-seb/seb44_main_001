1256e9a74ba087a4cf428f2014cb64d9e72a9a52
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2023-07-14 09:36:23 +0900

    Feat: 좋아요 기능 구현중

--- client/src/pages/Details/components/Article.tsx
+++ client/src/pages/Details/components/Article.tsx
@@ -1,5 +1,5 @@
 import { AiFillDelete } from 'react-icons/ai';
-import { useMutation, useQueryClient } from 'react-query';
+import { useMutation, useQuery, useQueryClient } from 'react-query';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import { styled } from 'styled-components';
 import deleteArticle from '../api/deleteArticle';
@@ -14,11 +14,10 @@ import { RootState } from '../../../common/store/RootStore';
 import { setCreatedPost } from '../../Write,Edit/store/CreatedPost';
 import { setLocation } from '../../../common/store/LocationStore';
 import { setCategory } from '../../../common/store/CategoryStore';
-import { useState } from 'react';
+import getLike from '../api/getLike';
+import postLike from '../api/postLike';
 
 export default function Article({ data }: { data?: ArticleToGet }) {
-  const [isLiked, setIsLiked] = useState(false);
-
   const queryClient = useQueryClient();
 
   const userInfo = { memberId: 2 };
@@ -30,6 +29,16 @@ export default function Article({ data }: { data?: ArticleToGet }) {
   const navigate = useNavigate();
 
   const totalComments = useSelector((state: RootState) => state.totalComments);
+
+  //해당 포스트의 좋아요 데이터중 나의 아이디와 일치하는 데이터 있는지 get요청
+  const { data: likeData } = useQuery(['getLike'], () =>
+    getLike(`${BASE_URL}/likes/${id}`, userInfo.memberId),
+  );
+
+  //postId와 memberId 요청에 보내서 있으면
+  const postLikeMutaion = useMutation(() =>
+    postLike(`${BASE_URL}/posts/${id}`, userInfo.memberId),
+  );
 
   const deleteItemMutation = useMutation<void, unknown, void>(
     () => deleteArticle(`${BASE_URL}/posts/${id}/${userInfo.memberId}`),
@@ -71,6 +80,11 @@ export default function Article({ data }: { data?: ArticleToGet }) {
     if (window.confirm('정말로 삭제하시겠습니까?')) {
       deleteItemMutation.mutate();
     }
+  };
+
+  const handleClickLike = () => {
+    console.log('조아요눌러찌');
+    postLikeMutaion.mutate();
   };
 
   return (
@@ -115,12 +129,13 @@ export default function Article({ data }: { data?: ArticleToGet }) {
             </div>
             <div>
               <div>
-                <Button type="button" onClick={()=>{
-                    console.log("좋아요클릭!")
-                }}>
-                  <img src={isLiked?`${peach_on}`:`${peach_off}`} alt="liked" />
+                <Button type="button" onClick={handleClickLike}>
+                  <img
+                    src={likeData ? `${peach_on}` : `${peach_off}`}
+                    alt={likeData ? 'Liked' : 'Not liked'}
+                  />
                 </Button>
-                <div>999</div>
+                <div>{data.likeCount}</div>
               </div>
               <div>
                 <img src={comment} alt="comment" />
