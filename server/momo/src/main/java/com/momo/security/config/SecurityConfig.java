6d4f376b97a475b94900ab33503882e4191b179e
rktdngud <124556344+rktdngud@users.noreply.github.com>
2023-07-18 11:50:34 +0900

    [BE] modify securityConfig

--- server/momo/src/main/java/com/momo/security/config/SecurityConfig.java
+++ server/momo/src/main/java/com/momo/security/config/SecurityConfig.java
@@ -108,7 +108,7 @@ public class SecurityConfig {
         public void configure(HttpSecurity builder) throws Exception {
             AuthenticationManager authenticationManager = builder.getSharedObject(AuthenticationManager.class);
 
-            JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(authenticationManager, jwtTokenizer, refreshTokenRepository);
+            JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(authenticationManager, jwtTokenizer);
             jwtAuthenticationFilter.setFilterProcessesUrl("/auth/login");
 
             JwtVerificationFilter jwtVerificationFilter = new JwtVerificationFilter(jwtTokenizer, authorityUtils, tokenBlacklistService);
