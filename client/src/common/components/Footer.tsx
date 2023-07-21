27b7e0666b1b221c5bde3db27d596475e8c3bd6b
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2023-07-21 09:27:55 +0900

    Chore: 호버시 아바타 띄우기

--- client/src/common/components/Footer.tsx
+++ client/src/common/components/Footer.tsx
@@ -1,17 +1,63 @@
 import { styled } from 'styled-components';
-import { Link } from 'react-router-dom';
+
+interface LinkWithTooltipProps {
+  $image: string;
+  backgroundImage?: string;
+  key:string;
+  href:string;
+  target:string;
+}
 
 export default function Footer() {
   const githubUrl = 'https://github.com/';
+  const users = [
+    {
+      name: '김진솔',
+      github: 'jinsoul75',
+      image: 'https://avatars.githubusercontent.com/u/80370226?v=4',
+    },
+    {
+      name: '박무생',
+      github: 'Mooobi',
+      image: 'https://avatars.githubusercontent.com/u/124570875?v=4',
+    },
+    {
+      name: '송혜수',
+      github: 'shyesoo',
+      image: 'https://avatars.githubusercontent.com/u/101123722?v=4',
+    },
+    {
+      name: '김윤',
+      github: 'Yooney1',
+      image: '',
+    },
+    {
+      name: '최우형',
+      github: 'rktdngud',
+      image: 'https://avatars.githubusercontent.com/u/124556344?v=4',
+    },
+    {
+      name: '조명인',
+      github: 'myoungincho729',
+      image: '',
+    },
+  ];
   return (
     <Wapper>
       <Content>
-        Copyright 2023. [ FE <Link to={`${githubUrl}jinsoul75`} target="_blank">김진솔</Link>{' '}
-        <Link to={`${githubUrl}Mooobi`} target="_blank">박무생</Link>{' '}
-        <Link to={`${githubUrl}shyesoo`} target="_blank">송혜수</Link> / BE{' '}
-        <Link to={`${githubUrl}Yooney1`} target="_blank">김윤</Link>{' '}
-        <Link to={`${githubUrl}rktdngud`} target="_blank">최우형</Link>{' '}
-        <Link to={`${githubUrl}myoungincho729`} target="_blank">조명인</Link> ]. All rights reserved.
+        Copyright 2023. [ FE{' '}
+        {users.map((user) => (
+          <LinkWithTooltip
+            key={user.github}
+            href={`${githubUrl}${user.github}`}
+            target="_blank"
+            $image={user.image}
+          >
+            {user.name}
+          </LinkWithTooltip>
+        ))}/
+
+        ]. All rights reserved.
       </Content>
     </Wapper>
   );
@@ -21,15 +67,36 @@ const Wapper = styled.footer`
   background-color: var(--color-white);
   display: flex;
   justify-content: center;
-  left: 0;
   bottom: 0;
   width: 100%;
-  position:absolute;
+  position: absolute;
+  border-top: 1px solid var(--color-gray);
 `;
 
 const Content = styled.div`
   padding: 1rem;
-  >a{
-    color:var(--color-black);
+  > a {
+    color: var(--color-black);
+  }
+`;
+
+const LinkWithTooltip = styled.a<LinkWithTooltipProps>`
+  position: relative;
+  display: inline-block;
+  &:hover::before {
+    content: '';
+    position: absolute;
+    bottom: 100%;
+    left: 50%;
+    transform: translateX(-50%);
+    z-index: 2;
+    display: block;
+    width: 100px;
+    height: 100px;
+    background-image: ${({ $image }) => `url('${$image}')`};
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+    border-radius: 4px;
   }
 `;
