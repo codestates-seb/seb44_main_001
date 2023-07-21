7d68880d4492f9521a219f15771fcf8bf94a9107
myoungin <myoungin729@gmail.com>
2023-07-21 15:42:43 +0900

    Fix: @GeneratedValue strategy -> AUTO로 변경

--- server/momo/src/main/java/com/momo/chat/entity/Message.java
+++ server/momo/src/main/java/com/momo/chat/entity/Message.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class Message {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue
     private Long id;
 
     private String message;
