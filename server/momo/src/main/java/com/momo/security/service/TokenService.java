e7f521cf6ae4e5ecee5b5ce7be496d79200fc580
myoungin <myoungin729@gmail.com>
2023-07-22 22:36:20 +0900

    Fix: tokenService에서 새 액세스토큰 만들때 username 필드 추가

--- server/momo/src/main/java/com/momo/security/service/TokenService.java
+++ server/momo/src/main/java/com/momo/security/service/TokenService.java
@@ -26,6 +26,7 @@ public class TokenService {
     public String delegateAccessToken(Member member) {
         Map<String, Object> claims = new HashMap<>();
         claims.put("memberId", member.getMemberId());
+        claims.put("username", member.getEmail());
         claims.put("roles", member.getRoles());
 
         String subject = member.getEmail();
