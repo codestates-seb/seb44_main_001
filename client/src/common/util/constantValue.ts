96cac9ed5fb601d3b6d3187537ae696cd875d908
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2023-07-23 14:34:24 +0900

    Chore: 배포 환경 설정

--- client/src/common/util/constantValue.ts
+++ client/src/common/util/constantValue.ts
@@ -1,5 +1,4 @@
-export const BASE_URL =
-  'http://ec2-54-180-137-136.ap-northeast-2.compute.amazonaws.com:8080';
+export const BASE_URL = 'https://momomomo.shop:8080';
 
 export const TITLE_INPUT_PLACEHOLDER = '내가 원하는 친구를 모집해봐요!';
 
