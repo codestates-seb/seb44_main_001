0cc04c74beaec3823f33f41ff3b9f9928b772676
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2023-08-09 13:51:45 +0900

    Remove: 유저,토큰 정보 상태 삭제

--- client/src/common/store/RootStore.tsx
+++ client/src/common/store/RootStore.tsx
@@ -6,10 +6,7 @@ import { selectedLocationSlice } from '../../pages/Lists,Search/store/SelectedLo
 import { createdPostSlice } from '../../pages/Write,Edit/store/CreatedPost';
 import { setSignupSlice } from '../../pages/Signup/store/SignupUser';
 import { setLoginSlice } from '../../pages/Login/store/LoginUser';
-import { memberSlice } from '../../pages/User/store/MemberStore';
 import { totalCommentsSlice } from './CommentPageStore';
-import { tokenSlice } from '../../pages/Login/store/userTokenStore';
-import { myDataSlice } from '../../pages/Login/store/MyUserData';
 import { chatModalSlice } from './ChatModalStore';
 import { updatedUserSlice } from '../../pages/Kakao-signup/store/UpdatedUserData';
 import { chatRoomInfoSlice } from './ChatRoomInfoStore';
@@ -23,9 +20,6 @@ const rootReducer = combineReducers({
   createdPost: createdPostSlice.reducer,
   signup: setSignupSlice.reducer,
   login: setLoginSlice.reducer,
-  token: tokenSlice.reducer,
-  myData: myDataSlice.reducer,
-  member: memberSlice.reducer,
   authSignup: updatedUserSlice.reducer,
   totalComments: totalCommentsSlice.reducer,
   chatRoomInfo: chatRoomInfoSlice.reducer,
