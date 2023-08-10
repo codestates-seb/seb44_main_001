661c4df197fd52fb026381a88c91cf3ff784158e
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2023-08-10 14:51:09 +0900

    Fix: catch error 로직수정

--- client/src/common/util/customHook/useMyInfo.tsx
+++ client/src/common/util/customHook/useMyInfo.tsx
@@ -1,4 +1,4 @@
-import { useQuery } from 'react-query';
+import { useQuery,useQueryClient } from 'react-query';
 import { AxiosError } from 'axios';
 import { useNavigate } from 'react-router-dom';
 
@@ -9,6 +9,7 @@ import { Member } from '../../type';
 export default function useMyInfo() {
   const navigate = useNavigate();
   const token = localStorage.getItem('Authorization');
+  const queryClient = useQueryClient();
   const {
     data: myData,
     isLoading,
@@ -18,20 +19,18 @@ export default function useMyInfo() {
     () => getData(`${BASE_URL}/members/userInfo`),
     {
       enabled: !!token,
-      onSuccess:(myData)=>{
-        console.log(myData);
-      },
       onError: (error) => {
         if (error.response?.status === 401 && token) {
           navigate('/');
           localStorage.clear();
+          queryClient.clear();
           alert('토큰이 만료되었습니다. 다시 로그인해주세요.');
         } else {
           console.error('오류가 발생했습니다.', error.message);
         }
       },
-      staleTime: Infinity,
-      cacheTime: Infinity,
+      staleTime: 30 * 60 * 1000,
+      cacheTime: 30 * 60 * 1000,
     },
   );
   return { myData, isLoading, error };
